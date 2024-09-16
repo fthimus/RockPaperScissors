bd41301c6064f2fd20f3a99dc3d0d4bf43b12da9
fthimus <fthimus@gmail.com>
2024-09-17 01:16:05 +0700

    Add function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const getComputerChoice = () => {
+    const seed = Math.random();
+    if (seed > (2 / 3))
+        return "Rock";
+    if (seed > (1 / 3) && seed < (2 / 3))
+        return "Paper";
+    return "Scissors";
+}
+
+console.log(getComputerChoice());
